7750429d1e9705e7b98701b6bb119fe41f3a4218
Sant090 <120807532+Sant090@users.noreply.github.com>
2023-08-04 19:17:14 -0500

    v2

--- foro/Foro.js
+++ foro/Foro.js
@@ -1,13 +1,29 @@
-function agregarPublicacion(event) {
-    event.preventDefault();
-    const contenido = document.getElementById("nuevaPublicacion").value;
-    if (contenido.trim() !== "") {
-        const publicacion = document.createElement("div");
-        publicacion.classList.add("Publicacion");
-        publicacion.textContent = contenido;
-        document.getElementById("Publicaciones").appendChild(publicacion);
-        document.getElementById("nuevaPublicacion").value = "";
+function addResponse() {
+    const responseText = document.getElementById("input-response").value.trim();
+    if (responseText === "") {
+      alert("Por favor, escribe una respuesta válida.");
+      return;
     }
-}
+    const date = new Date();
+    const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
 
-document.getElementById("Formulario").addEventListener("submit", agregarPublicacion);
+    const responseBox = document.createElement("div");
+    responseBox.classList.add("response-box");
+
+    const responseTextElement = document.createElement("p");
+    responseTextElement.classList.add("response-text");
+    responseTextElement.textContent = responseText;
+
+    const responseDateElement = document.createElement("span");
+    responseDateElement.classList.add("response-date");
+    responseDateElement.textContent = formattedDate;
+
+    responseBox.appendChild(responseTextElement);
+    responseBox.appendChild(responseDateElement);
+
+    const responsesContainer = document.getElementById("responses-container");
+    responsesContainer.appendChild(responseBox);
+
+    // Limpiar el cuadro de respuesta después de agregarlo
+    document.getElementById("input-response").value = "";
+  }
---
+++ proyecto.js
@@ -0,0 +1,25 @@
+let M1 = "Utilizar contraseñas fuertes y únicas para cada cuenta es crucial. Se deben evitar contraseñas obvias o comunes. Una contraseña fuerte debe tener una combinación de letras, números y caracteres especiales. Además, es recomendable utilizar una frase de contraseña en lugar de una sola palabra. También es esencial no reutilizar contraseñas en múltiples cuentas."
+let M2 = "Las personas deben ser cautelosas con correos electrónicos, mensajes o llamadas no solicitadas que solicitan información personal o financiera. Siempre que se reciba un mensaje sospechoso o se solicite información confidencial, es recomendable verificar la autenticidad del remitente o la fuente antes de proporcionar cualquier dato."
+let M3 = "Es fundamental tener precaución al conectarse a redes Wi-Fi públicas o no seguras, ya que estas pueden ser vulnerables a ataques y suplantación de identidad. Evitar el acceso a cuentas bancarias, correo electrónico u otros servicios que requieran información confidencial cuando se está en una red desconocida. Si es necesario utilizar redes públicas, es recomendable utilizar una red privada virtual (VPN) que cifre la conexión y proteja la información de posibles espías."
+
+
+let textos = [M1, M2, M3];
+let indice = 0;
+
+function rotarTexto() {
+  const Metodos = document.getElementById("Metodos");
+  indice = (indice + 1) % textos.length;
+  Metodos.textContent = textos[indice];
+}
+
+function retrocederTexto() {
+  const Metodos = document.getElementById("Metodos");
+  indice = (indice - 1 + textos.length) % textos.length;
+  Metodos.textContent = textos[indice];
+}
+
+function redireccionar() {
+  // Reemplaza "URL_DE_LA_OTRA_PAGINA" con la URL de la página dentro de la carpeta.
+  // Agregamos target="_blank" para que se abra en una nueva ventana.
+  window.open("Foro/Foro.html", "_blank");
+}
