1598dd0564f8b148938a3e39a85f2c349a0e2bec
Sant090 <120807532+Sant090@users.noreply.github.com>
2023-08-18 20:25:22 -0500

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -7,17 +7,42 @@ let textos = [M1, M2, M3];
 let indice = 0;
 
 function rotarTexto() {
-  const Metodos = document.getElementById("Metodos");
+  const Metodos = document.getElementById("metodos");
   indice = (indice + 1) % textos.length;
   Metodos.textContent = textos[indice];
 }
 
 function retrocederTexto() {
-  const Metodos = document.getElementById("Metodos");
+  const Metodos = document.getElementById("metodos");
   indice = (indice - 1 + textos.length) % textos.length;
   Metodos.textContent = textos[indice];
 }
 
 function redireccionar() {
   window.open("https://personaldatasecurity.github.io/Foro/", "_blank");
+}
+
+
+
+
+
+
+let V1 = "El phishing es una técnica en la que los delincuentes se hacen pasar por amigos, sitios de comercio electrónico o instituciones financieras para robar información personal y financiera de sus víctimas. Este tipo de ciberestafa representa el 27% de los casos en todo el mundo, según una encuesta de TransUnion. La estafa consiste en construir un sitio web falso que se parece extrañamente a los de varias empresas confiables. Los delincuentes envían un enlace solicitando la confirmación de los datos a través de mensajes enviados por correo electrónico, Whatsapp o SMS. Las víctimas que hacen clic en este enlace son llevadas al sitio web falso, donde caen en la trampa y dan su información."
+let V2 =" El Transmitir recibos bancarios falsos, Este tipo de delito cibernético implica la producción de recibos falsos en nombre de las grandes corporaciones. La víctima deposita los fondos en la cuenta de los estafadores durante la transacción. Algunas variaciones de este tipo de fraude pueden incluso involucrar la creación de páginas falsas que imiten el entorno de la empresa, donde las víctimas son redirigidas para descargar el documento falso sin generar ninguna señal de alarma sobre la estafa."
+let V3 ="La Instalacion de softwares maliciosos, Este tipo de fraude en línea puede ocurrir cuando un usuario visita sitios web dudosos o incluso cuando hace clic en enlaces de correos electrónicos dudosos. En estos escenarios, los usuarios corren el riesgo de instalar malware sin querer, lo que otorgaría a terceros acceso a su información privada, así como a datos financieros y personales."
+let V4 ="El Robo de información de la tarjeta, Este tipo de fraude puede ocurrir tanto físicamente (cuando le roban la tarjeta a un usuario) como virtualmente (cuando un usuario ingresa la información de la tarjeta en sitios web falsos, inseguros o inseguros). Una vez que tienen acceso a esta información, los estafadores pueden realizar compras hasta que el usuario bloquee la tarjeta. Robo de tarjetas de crédito, que representa el 13 % de los casos a nivel mundial y ocupa el noveno lugar entre los tipos de fraude digital más frecuentes, según la encuesta de TransUnion, junto con los cargos fraudulentos."
+
+let textos2 = [V1, V2, V3, V4];
+let indice2 = 0;
+
+function otrotexto() {
+  const textE = document.getElementById("textE");
+  indice2 = (indice2 + 1) % textos2.length;
+  textE.textContent = textos2[indice2];
+}
+
+function anteriortexto() {
+  const textE = document.getElementById("textE");
+  indice2 = (indice2 - 1 + textos2.length) % textos.length;
+  textE.textContent = textos2[indice2];
 }
