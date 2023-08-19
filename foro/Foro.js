0bcc7b572ddf80dd560474dccbf20134694beb04
Sant090 <120807532+Sant090@users.noreply.github.com>
2023-08-18 20:39:17 -0500

    Update Foro.js

--- foro/Foro.js
+++ foro/Foro.js
@@ -1,29 +1,29 @@
 function addResponse() {
-    const responseText = document.getElementById("input-response").value.trim();
-    if (responseText === "") {
-      alert("Por favor, escribe una respuesta válida.");
-      return;
-    }
-    const date = new Date();
-    const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-
-    const responseBox = document.createElement("div");
-    responseBox.classList.add("response-box");
-
-    const responseTextElement = document.createElement("p");
-    responseTextElement.classList.add("response-text");
-    responseTextElement.textContent = responseText;
-
-    const responseDateElement = document.createElement("span");
-    responseDateElement.classList.add("response-date");
-    responseDateElement.textContent = formattedDate;
-
-    responseBox.appendChild(responseTextElement);
-    responseBox.appendChild(responseDateElement);
-
-    const responsesContainer = document.getElementById("responses-container");
-    responsesContainer.appendChild(responseBox);
-
-
-    document.getElementById("input-response").value = "";
+  const responseText = document.getElementById("input-response").value.trim();
+  if (responseText === "") {
+    alert("Por favor, escribe una respuesta válida.");
+    return;
   }
+  const date = new Date();
+  const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+
+  const responseBox = document.createElement("div");
+  responseBox.classList.add("response-box");
+
+  const responseTextElement = document.createElement("p");
+  responseTextElement.classList.add("response-text");
+  responseTextElement.textContent = responseText;
+
+  const responseDateElement = document.createElement("span");
+  responseDateElement.classList.add("response-date");
+  responseDateElement.textContent = formattedDate;
+
+  responseBox.appendChild(responseTextElement);
+  responseBox.appendChild(responseDateElement);
+
+  const responsesContainer = document.getElementById("responses-container");
+  responsesContainer.appendChild(responseBox);
+
+
+  document.getElementById("input-response").value = "";
+}
